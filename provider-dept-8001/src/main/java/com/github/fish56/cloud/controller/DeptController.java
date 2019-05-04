37538b8e6331525609b138cee014eece281d877c
bitfishxyz@gmail.com <bitfishxyz@gmail.com>
2019-05-04 11:10:18 +0800

    dept provider从Eureka发现自身

--- provider-dept-8001/src/main/java/com/github/fish56/cloud/controller/DeptController.java
+++ provider-dept-8001/src/main/java/com/github/fish56/cloud/controller/DeptController.java
@@ -1,9 +1,12 @@
 package com.github.fish56.cloud.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.github.fish56.cloud.entity.Dept;
 import com.github.fish56.cloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 import java.util.List;
 
@@ -11,6 +14,9 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService service;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("/depts")
     public Boolean add(@RequestBody Dept dept){
@@ -25,5 +31,17 @@ public class DeptController {
     @GetMapping("/depts/{id}")
     public Dept dept(@PathVariable Long id){
         return service.findById(id);
+    }
+
+    @GetMapping("/depts/discovery")
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+
+        System.out.println("*******" + list);
+        List<ServiceInstance> instanceList2 = discoveryClient.getInstances("cloud-dept");
+        for (ServiceInstance instance : instanceList2){
+            System.out.println(JSONObject.toJSONString(instance));
+        }
+        return this.discoveryClient;
     }
 }
