8a9c114b6bce966f7894baa18719d0019e481aba
bitfishxyz@gmail.com <bitfishxyz@gmail.com>
2019-05-04 11:57:06 +0800

    dept comsumer 发现provider

--- consumer-dept-80/src/main/java/com/github/fish56/cloud/DeptConsumer80.java
+++ consumer-dept-80/src/main/java/com/github/fish56/cloud/DeptConsumer80.java
@@ -2,8 +2,11 @@ package com.github.fish56.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DeptConsumer80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer80.class, args);
--- consumer-dept-80/src/main/java/com/github/fish56/cloud/controller/DeptControllerConsumer.java
+++ consumer-dept-80/src/main/java/com/github/fish56/cloud/controller/DeptControllerConsumer.java
@@ -1,7 +1,10 @@
 package com.github.fish56.cloud.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.github.fish56.cloud.entity.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -31,5 +34,19 @@ public class DeptControllerConsumer {
     public List<Dept> getAll(Dept dept){
         return restTemplate.getForObject(
                 REST_URL_PROVIDER + "/depts", List.class);
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/consumer/depts/discovery")
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+
+        System.out.println("*******" + list);
+        List<ServiceInstance> instanceList2 = discoveryClient.getInstances("cloud-dept");
+        for (ServiceInstance instance : instanceList2){
+            System.out.println(JSONObject.toJSONString(instance));
+        }
+        return this.discoveryClient;
     }
 }
